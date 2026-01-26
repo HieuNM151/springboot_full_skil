5da91b08f52bcd5cd0ec57f20a52938049ce60e0
hieunm <hieunm151@gmail.com>
2026-01-26 22:02:10 +0700

    no message

---
+++ src/main/java/vn/hieunm/Hieunm151JavaApplication.java
@@ -0,0 +1,13 @@
+package vn.hieunm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hieunm151JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hieunm151JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hieunm/Hieunm151JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hieunm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hieunm151JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
